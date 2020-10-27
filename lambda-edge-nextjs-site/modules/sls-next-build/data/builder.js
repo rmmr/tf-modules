9d31eede17cc994d07ec095a311e8db48a00e10d
Raymond Reggers <raymond@adaptiv.nl>
2020-10-27 20:36:14 +0100

    wip

--- lambda-edge-nextjs-site/modules/sls-next-build/data/builder.js
+++ lambda-edge-nextjs-site/modules/sls-next-build/data/builder.js
@@ -3,8 +3,12 @@ const { Builder } = require("@sls-next/lambda-at-edge")
 
 const rootDir = process.cwd()
 
-const useServerlessTraceTarget = process.env.USE_SERVERLESS_TRACE_TARGETS === '1' ? true : false
 const minifyHandlers = process.env.MINIFY_HANDLERS === '1' ? true : false
+const useServerlessTraceTarget = process.env.USE_SERVERLESS_TRACE_TARGETS === '1' ? true : false
+
+console.log('building')
+console.log(minifyHandlers)
+console.log(useServerlessTraceTarget)
 
 const builder = new Builder(
   rootDir,
@@ -14,8 +18,8 @@ const builder = new Builder(
     cwd: rootDir,
     env: {},
     args: ['build'],
-    useServerlessTraceTarget,
     minifyHandlers,
+    useServerlessTraceTarget,
   }
 );
 
