ef405256ac1159c69587cfd7e15a3761fc7eb0ee
Raymond Reggers <raymond@adaptiv.nl>
2020-11-28 07:07:49 +0100

    wip

--- ssm-store/set/main.tf
+++ ssm-store/set/main.tf
@@ -2,5 +2,5 @@ resource "aws_ssm_parameter" "this" {
   for_each = var.values
   name     = "/${var.name}/${each.key}"
   type     = can(tostring(each.value)) ? "String" : "StringList"
-  value    = can(tostring(each.value)) ? value : join(",", each.value)
+  value    = can(tostring(each.value)) ? each.value : join(",", each.value)
 }
