9fe242eca8d28f332981ad739e464dd75f9bf48b
Raymond Reggers <raymond@adaptiv.nl>
2020-11-28 07:50:26 +0100

    wip

--- ssm-store/get/outputs.tf
+++ ssm-store/get/outputs.tf
@@ -1,5 +1,11 @@
-output "values" {
+output "string" {
   value = { for key in var.keys :
-    key => data.aws_ssm_parameter.this[key].type == "StringList" ? split(",", data.aws_ssm_parameter.this[key].value) : data.aws_ssm_parameter.this[key].value
+    key => data.aws_ssm_parameter.this[key].type == "String" ? data.aws_ssm_parameter.this[key].value : null
+  }
+}
+
+output "list" {
+  value = { for key in var.keys :
+    key => data.aws_ssm_parameter.this[key].type == "StringList" ? split(",", data.aws_ssm_parameter.this[key].value) : null
   }
 }
