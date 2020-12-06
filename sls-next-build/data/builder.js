e37a0f60234b1f1a69a12c13865a37abc345ef11
Raymond Reggers <raymond@adaptiv.nl>
2020-12-06 12:47:23 +0100

    wip

--- sls-next-build/data/builder.js
+++ sls-next-build/data/builder.js
@@ -7,22 +7,26 @@ const minifyHandlers = process.env.MINIFY_HANDLERS === '1' ? true : false
 const useServerlessTraceTarget = process.env.USE_SERVERLESS_TRACE_TARGETS === '1' ? true : false
 const domainRedirects = process.env.DOMAIN_REDIRECTS !== undefined ? JSON.parse(process.env.DOMAIN_REDIRECTS) : {}
 
-const builder = new Builder(
-  rootDir,
-  path.join(rootDir, ".serverless_next"),
-  {
-    cmd: './node_modules/.bin/next',
-    cwd: rootDir,
-    env: {},
-    args: ['build'],
-    minifyHandlers,
-    useServerlessTraceTarget,
-    domainRedirects,
-  }
-);
 
 async function main() {
+  console.log(`Building sls next at location "${rootDir}"`)
+  const builder = new Builder(
+    rootDir,
+    path.join(rootDir, ".serverless_next"),
+    {
+      cmd: './node_modules/.bin/next',
+      cwd: rootDir,
+      env: {},
+      args: ['build'],
+      minifyHandlers,
+      useServerlessTraceTarget,
+      domainRedirects,
+    }
+  );
+
   await builder.build()
 }
 
-main()
+main().catch(() => {
+  process.exit(1)
+})
