cb04c79a828c2e2232459558c7e06127917f797b
Raymond Reggers <raymond@adaptiv.nl>
2021-05-13 15:40:48 +0200

    wip

--- db/outputs.tf
+++ db/outputs.tf
@@ -2,6 +2,10 @@ output "db_proxy_endpoint" {
   value = var.create_db_proxy ? aws_db_proxy._.0.endpoint : null
 }
 
+output "db_cluster_endpoint" {
+  value = aws_rds_cluster._.endpoint
+}
+
 output "db_cluster_instance_endpoints" {
   value = aws_rds_cluster_instance._.*.endpoint
 }
